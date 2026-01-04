f8e752c7c0cb52b4df4fd6fdffc5980c267258dc
oleg-torba <100319055+oleg-torba@users.noreply.github.com>
2026-01-04 17:17:44 +0200

    some

--- models/warranty.js
+++ models/warranty.js
@@ -1,54 +1,34 @@
 const { Schema, model } = require("mongoose");
 
 const WarrantySchema = new Schema({
-  repairNumber: {
-    type: String,
-    required: true,
-  },
-  certificateNumber: {
-    type: String,
-    required: true,
-  },
-  part: {
-    type: String,
-    required: true,
-  },
-  saleDate: {
-    type: Date,
-    required: true,
-  },
-  reporting: {
-    type: String,
-    required: true,
-  },
-  imageUrl: {
-    type: String,
-    required: true,
-  },
-  manager: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  brand: {
-    type: String,
-    required: true,
-  },
-  fixationDate: {
-    type: Date,
-    default: null,
-  },
-  rezolution: {
-    type: String,
-    default: "",
-  },
+  repairNumber: { type: String, required: true }, // завжди потрібен
+  createdBy: { type: String, enum: ["master", "manager"], required: true },
+  createdAt: { type: Date, default: Date.now },
+
+  // --- MASTER ---
+  warrantyVerdict: { type: String, enum: ["Гарантія", "Не гарантія"], default: null },
+  masterImages: [
+    {
+      url: String,
+      public_id: String,
+    }
+  ],
+
+  // --- MANAGER ---
+  certificateNumber: { type: String, default: null },
+  part: { type: String, default: null },
+  saleDate: { type: Date, default: null },
+  reporting: { type: String, default: null },
+  imageUrl: { type: String, default: null }, // одне фото менеджера
+  manager: { type: String, default: null },
+  brand: { type: String, default: null },
+
+  // --- STATUS ---
+  rezolution: { type: String, enum: ["","ok","rejected"], default: "" },
+  fixationDate: { type: Date, default: null },
   autoApproved: { type: Boolean, default: false },
   public_id: String,
 });
 
 const Warranty = model("warranty", WarrantySchema);
-
 module.exports = Warranty;
