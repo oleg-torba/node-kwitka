d3d2ce4e0bd155da755d26e16b73695016ef1d1e
oleg-torba <100319055+oleg-torba@users.noreply.github.com>
2026-03-21 18:17:57 +0200

    Update warranty.js

--- controllers/warranty.js
+++ controllers/warranty.js
@@ -6,11 +6,15 @@ const nodemailer = require("nodemailer");
 
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  family: 4,
+  connectionTimeout: 10000,
 });
 
 transporter.verify(function (error, success) {
